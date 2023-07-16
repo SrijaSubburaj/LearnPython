b75a99295a86f7aca5d4605b88a0de9d8e440d11
SrijaSubburaj <139329994+SrijaSubburaj@users.noreply.github.com>
2023-07-16 22:41:18 +0530

    Update cleanse.py

--- cleanse.py
+++ cleanse.py
@@ -7,7 +7,6 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 credential = DefaultAzureCredential()
 blob_service_client = BlobServiceClient(account_url="https://salearn220623.blob.core.windows.net/",credential="96rxu4rzwCrz0hpaD/Dr9gvs5WJPXAo6pl1feH3xyth6Z9NCkWcNDzKwTuxU0/VMeOm9pJKWgEzO+AStWlnjiQ==")
   
-
 def convert_csv_to_parquet(parquet_file):
     blob_client = blob_service_client.get_blob_client(container="raw", blob=csv_file)
     csv_data = blob_client.download_blob().content_as_text()
