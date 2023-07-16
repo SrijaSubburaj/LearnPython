881a20964d6ba2b89effb25f1311add97cc15b83
SrijaSubburaj <139329994+SrijaSubburaj@users.noreply.github.com>
2023-07-16 22:40:41 +0530

    Update cleanse.py

--- cleanse.py
+++ cleanse.py
@@ -3,11 +3,12 @@ import pandas as pd
 from azure.storage.blob import BlobServiceClient
 from azure.identity import DefaultAzureCredential
 from io import StringIO
-
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+credential = DefaultAzureCredential()
+blob_service_client = BlobServiceClient(account_url="https://salearn220623.blob.core.windows.net/",credential="96rxu4rzwCrz0hpaD/Dr9gvs5WJPXAo6pl1feH3xyth6Z9NCkWcNDzKwTuxU0/VMeOm9pJKWgEzO+AStWlnjiQ==")
+  
+
 def convert_csv_to_parquet(parquet_file):
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=salearn220623;AccountKey=96rxu4rzwCrz0hpaD/Dr9gvs5WJPXAo6pl1feH3xyth6Z9NCkWcNDzKwTuxU0/VMeOm9pJKWgEzO+AStWlnjiQ==;EndpointSuffix=core.windows.net"
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     blob_client = blob_service_client.get_blob_client(container="raw", blob=csv_file)
     csv_data = blob_client.download_blob().content_as_text()
     data = pd.read_csv(StringIO(csv_data))
