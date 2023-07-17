094884a29a06cc85b870072c1b7dad3ce0bc7709
SrijaSubburaj <139329994+SrijaSubburaj@users.noreply.github.com>
2023-07-17 06:54:19 +0530

    Update add.py

--- add.py
+++ add.py
@@ -12,5 +12,5 @@ blob_service_client = BlobServiceClient(
 blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 blob_content=blob_client.download_blob().content_as_text()
 csv_data=StringIO(blob_content)
-df = pd.read_csv(csv_data)
+df = pd.read_csv(csv_data)  #reads the csv data
 print(df)
