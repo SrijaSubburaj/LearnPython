435679de7b2f3b5fca6fa2576a71abc38c2931d4
Ram Prasad N <narayanan.ramprasad@in.bosch.com>
2023-07-25 09:47:24 +0530

    rename file

--- readcsv.py
+++ readcsv.py
@@ -12,5 +12,4 @@ blob_client = blob_service_client.get_blob_client(container=container_name, blob
 blob_content=blob_client.download_blob().content_as_text()
 csv_data=StringIO(blob_content)
 df = pd.read_csv(csv_data)
-df = pd.read_csv(csv_data)  #reads the csv data
 print(df)
